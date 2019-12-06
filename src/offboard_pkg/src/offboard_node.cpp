375760070433a542f1b2d8399e9992c536a6c40e
zhufengting <562141235@qq.com>
2019-12-06 17:05:58 +0800

    second

--- src/offboard_pkg/src/offboard_node.cpp
+++ src/offboard_pkg/src/offboard_node.cpp
@@ -9,10 +9,36 @@
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
+//#include <offboard_node.h>
 
+
+
+
+//ｆｕｎｃｔｉｏｎ：消息接受函数
+//接受消息主要是当前飞机状态
 mavros_msgs::State current_state;
 void state_cb(const mavros_msgs::State::ConstPtr& msg){
     current_state = *msg;
+}
+
+//这个消息主要接收的是飞机的实时状态数据
+
+geometry_msgs::PoseStamped current_local_pos;
+void local_pos_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
+    current_local_pos = *msg;
+
+}
+
+
+//设置一个函数主要目的：检测飞机目标点和实际点的距离　返回值是一个ｂｏｏl型，因此可以直接通过ｉｆ语句调用
+
+bool pos_reached(geometry_msgs::PoseStamped current_pos, geometry_msgs::PoseStamped target_pos){
+
+    float err_px = current_pos.pose.position.x - target_pos.pose.position.x;
+    float err_py = current_pos.pose.position.y - target_pos.pose.position.y;
+    float err_pz = current_pos.pose.position.z - target_pos.pose.position.z;
+    ROS_INFO("pose.z = %.2f", sqrt(err_px * err_px + err_py * err_py + err_pz * err_pz));
+    return sqrt(err_px * err_px + err_py * err_py + err_pz * err_pz) < 0.08f;
 }
 
 
@@ -24,6 +50,8 @@ int main(int argc, char **argv)
 
     ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
             ("mavros/state", 10, state_cb);
+    ros::Subscriber local_position_sub = nh.subscribe<geometry_msgs::PoseStamped>
+            ("mavros/local_position/pose", 10, local_pos_cb);
     ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
             ("mavros/setpoint_position/local", 10);
     ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
@@ -41,9 +69,9 @@ int main(int argc, char **argv)
     }
 
     geometry_msgs::PoseStamped pose;
-    pose.pose.position.x = 0;
-    pose.pose.position.y = 0;
-    pose.pose.position.z = 2;
+    pose.pose.position.x = 4;
+    pose.pose.position.y = 4;
+    pose.pose.position.z = 5;
 
     //send a few setpoints before starting
     for(int i = 100; ros::ok() && i > 0; --i){
@@ -51,7 +79,6 @@ int main(int argc, char **argv)
         ros::spinOnce();
         rate.sleep();
     }
-
     mavros_msgs::SetMode offb_set_mode;
     offb_set_mode.request.custom_mode = "OFFBOARD";
 
@@ -84,10 +111,9 @@ int main(int argc, char **argv)
         ros::spinOnce();
         rate.sleep();
 
-
-
-
-        if( (ros::Time::now() - last_request > ros::Duration(30.0))) break;
+        //if( (ros::Time::now() - last_request > ros::Duration(30.0))) break;
+        //如果你想要多个点可以在这个ｉｆ下面重新设点
+        if(pos_reached(current_local_pos,pose)) break;
     }
 
 
---
+++ src/offboard_pkg/src/offboard_node.h
@@ -0,0 +1,11 @@
+//
+// Created by zft on 19-12-6.
+//
+
+#ifndef PROJECT_OFFBOARD_NODE_H
+#define PROJECT_OFFBOARD_NODE_H
+
+
+
+
+#endif //PROJECT_OFFBOARD_NODE_H
